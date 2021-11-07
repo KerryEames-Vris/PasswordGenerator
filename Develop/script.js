19cfe60def3b3ac99646a11b6884e48404500a32
Kerry Eames <Kerryeames@live.com>
2021-11-06 21:02:16 -0600

    made function to prompt password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["[", "]", "{", "}", "|", "/", "?", ".", ">", ",", "<", "@", "!", ":", ";", "#", "~", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,14 +15,11 @@ function writePassword() {
 
 // create generatePassword function
 function generatePassword() { 
+  var passwordLength = prompt("Please specify password length, between 10 and 120:", "Enter Password Length");
   
-}
+} 
 
-var passwordLength = ("");
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbols = ["[", "]", "{", "}", "|", "/", "?", ".", ">", ",", "<", "@", "!", ":", ";", "#", "~", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
+
 
 //prompt user for length
 // error handling - did they enter a valid number
