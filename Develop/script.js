2dbcaca23617af249ee969d6baf97d7e133cdde7
Kerry Eames <Kerryeames@live.com>
2021-11-06 22:42:16 -0600

    found coding to generate random string, but dont seem to be getting any output whatsoever from generatePassword. Troubleshooting

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var symbols = ["[", "]", "{", "}", "|", "/", "?", ".", ">", ",", "<", "@", "!", ":", ";", "#", "~", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,29 +14,55 @@ function writePassword() {
 }
 
 // create generatePassword function
-function generatePassword() { 
+function generatePassword() {
   var passwordLength = prompt("Please specify password length, between 8 and 128:", "Enter Password Length");
-  if(passwordLength < 8 && passwordLength < 128){
+  // Number(passwordLength)
+  
+  //prompt user for length
+  if(passwordLength < 8 || passwordLength > 128){
+    // error handling 
     alert("Please choose an option between 8 and 128!")
     generatePassword();
   } else {
-    var lowerAlpha = confirm("Would you like to use lowercase letters?")
+    // prompt user to confirm all character selections
+  var lowerAlpha = confirm("Would you like to use lowercase letters?");
+  }; 
+  if(lowerAlpha === true || false){
+     var upperAlpha = confirm("Would you like to use uppercase letters?");
   };
-  var upperAlpha = confirm("Would you like to use uppercase letters?");
-  var wantNumbers = confirm("Would you like to use numbers?");
-  var wantSymbols = confirm("Would you like to use symbols?");
+  if(upperAlpha === true || false){
+var wantNumbers = confirm("Would you like to use numbers?");
+  };
+  if(wantNumbers === true || false){
+var wantSymbols = confirm("Would you like to use symbols?");
+  };
+  if(wantSymbols === true || false){
+newPassword();
+  };
+  
+  
+  function newPassword() {
+   if(lowerAlpha === true && upperAlpha === true && wantNumbers === true && wantSymbols === true){
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.passwordLength;
+    for ( var i = 0; i < passwordLength; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
 }
+}
+}
+
+//use inputs to generate password
  
 
 
 
-//prompt user for length
-// error handling - did they enter a valid number
 
-// prompt use to confirm all character selections
 // error handling - did the user select at least one option
 
-//use inputs to generate password
+
 // return the password form the generatePassword
 
 // Add event listener to generate button
