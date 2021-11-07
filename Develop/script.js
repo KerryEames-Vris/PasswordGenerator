a0c6c5a7f9f6900a66da66c0db6feff5d234469b
Kerry Eames <Kerryeames@live.com>
2021-11-06 23:24:56 -0600

    was able to make function newPassword generate random string based on user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbols = ["[", "]", "{", "}", "|", "/", "?", ".", ">", ",", "<", "@", "!", ":", ";", "#", "~", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
+
+//Variables to appear in password
+var lowerCase = ["a" + "b" + "c" + "d" + "e" + "f" + "g" + "h" + "i" + "j" + "k" + "l" + "m" + "n" + "o" + "p" + "q" + "r" + "s" + "t" + "u" + "v" + "w" + "x" + "y" + "z"];
+var upperCase = ["A" + "B" + "C" + "D" + "E" + "F" + "G" + "H" + "I" + "J" + "K" + "L" + "M" + "N" + "O" + "P" + "Q" + "R" + "S" + "T" + "U" + "V" + "W" + "X" + "Y" + "Z"];
+var numbers = [0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9];
+var symbols = ["[" + "]" + "{" + "}" + "|" + "/" + "?" + "." + ">" + "," + "<" + "@" + "!" + ":" + ";" + "#" + "~" + "$" + "%" + "^" + "&" + "*" + "-" + "_" + "=" + "+"];
 
 // Write password to the #password input
 function writePassword() {
@@ -16,10 +18,10 @@ function writePassword() {
 // create generatePassword function
 function generatePassword() {
   var passwordLength = prompt("Please specify password length, between 8 and 128:", "Enter Password Length");
-  // Number(passwordLength)
+  Number(passwordLength)
   
   //prompt user for length
-  if(passwordLength < 8 || passwordLength > 128){
+  if(passwordLength < 8 || passwordLength > 128 || passwordLength === false){
     // error handling 
     alert("Please choose an option between 8 and 128!")
     generatePassword();
@@ -27,32 +29,37 @@ function generatePassword() {
     // prompt user to confirm all character selections
   var lowerAlpha = confirm("Would you like to use lowercase letters?");
   }; 
-  if(lowerAlpha === true || false){
+  if(lowerAlpha === true || lowerAlpha === false){
      var upperAlpha = confirm("Would you like to use uppercase letters?");
   };
-  if(upperAlpha === true || false){
+  if(upperAlpha === true || upperAlpha === false){
 var wantNumbers = confirm("Would you like to use numbers?");
   };
-  if(wantNumbers === true || false){
+  if(wantNumbers === true || wantNumbers === false){
 var wantSymbols = confirm("Would you like to use symbols?");
   };
-  if(wantSymbols === true || false){
-newPassword();
+  if(wantSymbols === true || wantSymbols === false){
+ return newPassword();
   };
+ 
   
   
   function newPassword() {
+
    if(lowerAlpha === true && upperAlpha === true && wantNumbers === true && wantSymbols === true){
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.passwordLength;
+    var characters = lowerCase + upperCase + numbers + symbols;
+    var charactersLength = characters.length;
     for ( var i = 0; i < passwordLength; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * 
  charactersLength));
    }
+   return result;
 }
+  }
 }
-}
+
+
 
 //use inputs to generate password
  
